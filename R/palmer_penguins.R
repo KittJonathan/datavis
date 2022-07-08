8fb60a34ccada408a901d99f6cbe22a4c2e3a599
codons-blog <codons@protonmail.com>
2022-07-08 14:48:38 +0200

    update

--- R/palmer_penguins.R
+++ R/palmer_penguins.R
@@ -8,17 +8,13 @@ library(tidyverse)
 
 # Scatter plot ----
 
-ggplot(data = penguins,
-       aes(x = bill_length_mm,
-           y = bill_depth_mm)) +
-  geom_point(aes(colour = species),
+(p1 <- ggplot() +
+  geom_point(data = penguins,
+             aes(x = bill_length_mm,
+                 y = bill_depth_mm,
+                 colour = species),
              size = 4,
              alpha = 0.5) +
-  # geom_rug(aes(colour = species),
-  #          outside = TRUE,
-  #          sides = "tr",
-  #          length = unit(0.02, "npc")) +
-  coord_cartesian(clip = "off") +
   scale_colour_manual(values = c("Adelie" = "darkorange",
                                "Chinstrap" = "purple",
                                "Gentoo" = "cyan4")) +
@@ -30,6 +26,8 @@ ggplot(data = penguins,
         axis.title.y = element_text(colour = "#4c5c94", size = 15),
         axis.text.x = element_text(colour = "#4c5c94"),
         axis.text.y = element_text(colour = "#4c5c94"),
+        legend.background = element_rect(fill = "#cde4f3"),
+        legend.key = element_rect(fill = "#cde4f3"),
         panel.background = element_rect(fill = "#cde4f3"),
         panel.border = element_rect(colour = "#3bc0f9"),
         panel.grid = element_line(colour = "#e9f5f6"),
@@ -37,3 +35,17 @@ ggplot(data = penguins,
         plot.background = element_rect(fill = "#cde4f3"),
         plot.title = element_text(size = 25, colour = "#4c5c94",
                                   margin = margin(b = 40)))
+)
+
+ggsave("figs/p1.png", p1, dpi = 320, width = 12, height = 6)
+
+# Map ----
+
+antarctica <- map_data("world") %>% 
+  filter(region == "Antarctica")
+
+ggplot(antarctica) +
+  geom_polygon(aes(x = long, y = lat, group = group),
+               fill = "#355c7d", colour = "white")
++
+  coord_fixed(1.3)
