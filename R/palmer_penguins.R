19918f2a2ff5ee0102265ed2845423f8297822ac
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-07-08 10:34:02 +0200

    testing geom_rug()

--- R/palmer_penguins.R
+++ R/palmer_penguins.R
@@ -15,7 +15,9 @@ ggplot(data = penguins,
              size = 4,
              alpha = 0.5) +
   geom_rug(aes(colour = species),
-           outside = TRUE) +
+           outside = TRUE,
+           sides = "tr",
+           length = unit(0.02, "npc")) +
   coord_cartesian(clip = "off") +
   scale_colour_manual(values = c("Adelie" = "darkorange",
                                "Chinstrap" = "purple",
@@ -24,13 +26,14 @@ ggplot(data = penguins,
        x = "Bill length (in mm)",
        y = "Bill depth (in mm)") +
   theme_light() +
-  theme(axis.title.x = element_text(colour = "#4c5c94", size = 15, margin = margin(t = 10)),
-        axis.title.y = element_text(colour = "#4c5c94", size = 15, margin = margin(r = 10)),
-        axis.text.x = element_text(colour = "#4c5c94", margin = margin(t = 20)),
-        axis.text.y = element_text(colour = "#4c5c94", margin = margin(r = 20)),
+  theme(axis.title.x = element_text(colour = "#4c5c94", size = 15),
+        axis.title.y = element_text(colour = "#4c5c94", size = 15),
+        axis.text.x = element_text(colour = "#4c5c94"),
+        axis.text.y = element_text(colour = "#4c5c94"),
         panel.background = element_rect(fill = "#cde4f3"),
         panel.border = element_rect(colour = "#3bc0f9"),
-        plot.margin = margin(0, 0, 25, 25),
         panel.grid = element_line(colour = "#e9f5f6"),
+        panel.grid.minor = element_blank(),
         plot.background = element_rect(fill = "#cde4f3"),
-        plot.title = element_text(size = 25, colour = "#4c5c94"))
+        plot.title = element_text(size = 25, colour = "#4c5c94",
+                                  margin = margin(b = 40)))
