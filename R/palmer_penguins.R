49cd8501c897b358d7c06c5255dd086d4899a403
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-07-08 09:40:33 +0200

    update

--- R/palmer_penguins.R
+++ R/palmer_penguins.R
@@ -12,11 +12,20 @@ ggplot(data = penguins,
        aes(x = bill_length_mm,
            y = bill_depth_mm)) +
   geom_point(aes(colour = species),
-             size = 4) +
+             size = 4,
+             alpha = 0.5) +
+  geom_rug(aes(colour = species)) +
   scale_colour_manual(values = c("Adelie" = "darkorange",
                                "Chinstrap" = "purple",
                                "Gentoo" = "cyan4")) +
-  labs(title = "Palmer Penguins") +
-  theme(panel.background = element_rect(fill = "#cde4f3"),
+  labs(title = "Palmer Penguins",
+       x = "Bill length (in mm)",
+       y = "Bill depth (in mm)") +
+  theme_light() +
+  theme(axis.title.x = element_text(colour = "#4c5c94", size = 15, margin = margin(t = 10)),
+        axis.title.y = element_text(colour = "#4c5c94", size = 15, margin = margin(r = 10)),
+        panel.background = element_rect(fill = "#cde4f3"),
+        panel.border = element_rect(colour = "#3bc0f9"),
+        panel.grid = element_line(colour = "#e9f5f6"),
         plot.background = element_rect(fill = "#cde4f3"),
         plot.title = element_text(size = 25, colour = "#4c5c94"))
