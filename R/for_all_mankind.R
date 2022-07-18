c20bf16b3d3fac2e8f06d52408290f6accaffc86
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-07-18 10:08:28 +0200

    update

--- R/for_all_mankind.R
+++ R/for_all_mankind.R
@@ -24,7 +24,7 @@ ggplot() +
   geom_circle(aes(x0 = 100, y0 = 25, r = 25)) +
   geom_segment(aes(x = 0, xend = 50, y = seq(5, 45, 4), yend = seq(5, 45, 4))) +
   geom_segment(aes(x = -25, xend = 0, y = 25, yend = seq(5, 45, 4)),
-               size = 2) +
+               size = 1) +
   # geom_arc(aes(x0 = 0, y0 = 45, r = 5, start = -150, end = 0)) +
   # geom_segment(aes(x = 0, xend = 5, y = 0, yend = 5)) +
   # geom_arc(aes(x0 = 15, y0 = 15, r = 4, start = 0, end = 2)) +
